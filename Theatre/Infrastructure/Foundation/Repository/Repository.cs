480b29e522031e517ef005876d029aeb46dda545
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-07-24 14:58:12 +0300

    Реализовал репозиторий и добавил указания программе для подключения к бд и использование нужного репозитория

---
+++ Theatre/Infrastructure/Foundation/Repository/Repository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Repositories;
+
+namespace Infrastructure.Foundation.Repository
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly TheaterDbContext _dbContext;
+
+        public Repository( TheaterDbContext dbContext )
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Create( T item )
+        {
+            _dbContext.Set<T>().Add( item );
+            _dbContext.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _dbContext.Set<T>().ToList();
+        }
+
+        public void Remove( T item )
+        {
+            _dbContext.Set<T>().Remove( item );
+        }
+    }
+}
--- Theatre/Theatre/Program.cs
+++ Theatre/Theatre/Program.cs
@@ -1,3 +1,8 @@
+using Domain.Repositories;
+using Infrastructure.Foundation;
+using Infrastructure.Foundation.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder( args );
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TheaterDbContext>( options =>
+{
+    options.UseSqlServer( "Server=LAPTOP-U0R8E398\\SQLEXPRESS;Database=Theatre;Trusted_Connection=True;TrustServerCertificate=True;" );
+} );
+
+builder.Services.AddScoped( typeof( IRepository<> ), typeof( Repository<> ) );
 
 var app = builder.Build();
 
