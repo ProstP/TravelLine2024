4c3b911d4b507436db1054db010f95a88d070b20
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-07-24 10:34:14 +0300

    Сделал валидацию для полей и списка режима работ для театра и добавил тесты

--- Theatre/Domain/Entities/Theatre.cs
+++ Theatre/Domain/Entities/Theatre.cs
@@ -8,7 +8,7 @@ public class Theatre
     public string Description { get; private set; }
     public string PhoneNumber { get; private set; }
 
-    public List<WorkingHours> WorkingHours { get; private set; } = new();
+    private List<WorkingHours> _workingHours = new();
 
     public List<Play> Plays { get; private set; } = new();
 
@@ -19,10 +19,26 @@ public class Theatre
         string description,
         string phoneNumber )
     {
+        if ( String.IsNullOrWhiteSpace( name ) )
+        {
+            throw new ArgumentException( $"{nameof( name )} can't be null or whitespace." );
+        }
         Name = name;
+        if ( String.IsNullOrWhiteSpace( address ) )
+        {
+            throw new ArgumentException( $"{nameof( address )} can't be null or whitespace." );
+        }
         Address = address;
         OpeningDate = openingDate;
+        if ( String.IsNullOrWhiteSpace( description ) )
+        {
+            throw new ArgumentException( $"{nameof( description )} can't be null or whitespace." );
+        }
         Description = description;
+        if ( String.IsNullOrWhiteSpace( phoneNumber ) )
+        {
+            throw new ArgumentException( $"{nameof( phoneNumber )} can't be null or whitespace." );
+        }
         PhoneNumber = phoneNumber;
     }
 
@@ -43,5 +59,60 @@ public class Theatre
         {
             PhoneNumber = phoneNumber;
         }
+    }
+
+    public IReadOnlyList<WorkingHours> WorkingHours
+    {
+        get
+        {
+            return _workingHours;
+        }
+    }
+
+    public void AddNewWorkingHours( WorkingHours workingHours )
+    {
+        byte newValidFrom = workingHours.ValidFrom;
+        byte newValidUntil = workingHours.ValidUntil;
+        if ( newValidFrom > newValidUntil )
+        {
+            newValidUntil += 7;
+        }
+
+        foreach ( var wh in _workingHours )
+        {
+            byte validFrom = wh.ValidFrom;
+            byte validUntil = wh.ValidUntil;
+
+            if ( validFrom > validUntil )
+            {
+                validUntil += 7;
+            }
+
+            if ( newValidUntil > 6 && validUntil < newValidFrom )
+            {
+                validFrom += 7;
+                validUntil += 7;
+            }
+            if ( validUntil > 6 && newValidUntil < validFrom )
+            {
+                newValidFrom += 7;
+                newValidUntil += 7;
+            }
+
+            if ( validFrom <= newValidFrom && newValidFrom <= validUntil
+                || validFrom <= newValidUntil && newValidUntil <= validUntil
+                || newValidFrom <= validFrom && validFrom <= newValidUntil
+                || newValidFrom <= validUntil && validUntil <= newValidUntil )
+            {
+                throw new ArgumentException( "Workinh hours can't overlap" );
+            }
+        }
+
+        _workingHours.Add( workingHours );
+    }
+
+    public void RemoveWorkingHours( WorkingHours workingHours )
+    {
+        _workingHours.Remove( workingHours );
     }
 }
