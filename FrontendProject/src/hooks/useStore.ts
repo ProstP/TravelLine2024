ca193c42218cfd59e6992b1a39da4e3d8cab2b01
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-08-21 07:15:37 +0300

    Установил библиотеку zustand и реализовал store

---
+++ FrontendProject/src/hooks/useStore.ts
@@ -0,0 +1,81 @@
+import { create } from "zustand";
+import { Application } from "../Entities/Application";
+import { Deck } from "../Entities/Deck";
+import { Card } from "../Entities/Card";
+
+type StoreData = {
+  app: Application;
+  actions: {
+    addDeck: (deck: Deck) => void;
+    deleteDeck: (id: string) => void;
+    addCardToDeck: (card: Card, idDeck: string) => void;
+    deleteCardInDeck: (idCard: string, idDeck: string) => void;
+    editCard: (word: string, translation: string, idCard: string, idDeck: string) => void;
+  };
+};
+
+export const useStore = create<StoreData>((set, get) => ({
+  app: {
+    decks: [],
+  },
+  actions: {
+    addDeck: (deck: Deck) => set({ ...get(), app: Application.AddNewDeck(deck, get().app) }),
+    deleteDeck: (id: string) => set({ ...get(), app: Application.DeleteDeck(id, get().app) }),
+
+    addCardToDeck(card, idDeck) {
+      const decks = [...get().app.decks];
+      const deckIndex = decks.findIndex(d => d.id === idDeck);
+      if (deckIndex === -1) {
+        return;
+      }
+
+      decks[deckIndex] = Deck.AddNewCard(card, decks[deckIndex]);
+
+      set({
+        ...get(),
+        app: {
+          decks: decks,
+        },
+      });
+    },
+    deleteCardInDeck(idCard, idDeck) {
+      const decks = [...get().app.decks];
+      const deckIndex = decks.findIndex(d => d.id === idDeck);
+      if (deckIndex === -1) {
+        return;
+      }
+
+      decks[deckIndex] = Deck.DeleteCard(idCard, decks[deckIndex]);
+
+      set({
+        ...get(),
+        app: {
+          decks: decks,
+        },
+      });
+    },
+
+    editCard(word, translation, idCard, idDeck) {
+      const decks = [...get().app.decks];
+      const deckIndex = decks.findIndex(d => d.id === idDeck);
+      if (deckIndex === -1) {
+        return;
+      }
+
+      const deck = decks[deckIndex];
+      const cardIndex = deck.cards.findIndex(c => c.id === idCard);
+      if (cardIndex === -1) {
+        return;
+      }
+
+      deck.cards[cardIndex] = Card.EditCard(word, translation, deck.cards[cardIndex]);
+
+      set({
+        ...get(),
+        app: {
+          decks: decks,
+        },
+      });
+    },
+  },
+}));
